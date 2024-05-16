66f1b9f49bdec6070dbf67ff22135e9a1e036087
shofiyul fuad <shofiyulfuad797@gmail.com>
2024-05-16 11:25:32 +0700

    qrcode

--- app/page.js
+++ app/page.js
@@ -1,10 +1,34 @@
-// import Login from "@/components/Login";
-import Dashboard from "./dashboard/page";
+// import Dashboard from "./dashboard/page";
 
-export default function Home() {
+// export default function Home() {
+//   return (
+//     <>
+//       {/* <Dashboard /> */}
+
+//     </>
+//   );
+// }
+
+import { useState, useEffect } from "react";
+
+const Home = () => {
+  const [seconds, setSeconds] = useState(30);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (seconds > 0) {
+        setSeconds((prevSeconds) => prevSeconds - 1);
+      }
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [seconds]);
+
   return (
-    <>
-      <Dashboard />
-    </>
+    <div>
+      <h1>Timer: {seconds} seconds</h1>
+    </div>
   );
-}
+};
+
+export default Home;
